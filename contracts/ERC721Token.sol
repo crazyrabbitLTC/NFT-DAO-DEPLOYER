44e90c7cb5228c80c050231ffb97790e72797873
Dennison Bertram <dennison@dennisonbertram.com>
2023-11-30 14:16:45 -0500

    Create deployment tasks, update code

--- contracts/ERC721Token.sol
+++ contracts/ERC721Token.sol
@@ -9,35 +9,51 @@ import "@openzeppelin/contracts/access/AccessControl.sol";
 import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Votes.sol";
 
+/// @title ERC721 Token Implementation
+/// @notice Extends ERC721 with additional features like enumeration, URI storage, burnability, access control, and voting.
+/// @dev Utilizes OpenZeppelin contracts for standardized, secure implementation of ERC721 functionality.
 /// @custom:security-contact dennison@tally.xyz
 contract ERC721Token is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, AccessControl, EIP712, ERC721Votes {
     bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
     uint256 private _nextTokenId;
 
-    string private baseURI = "";
+    string private _baseURI;
     event BaseURIChanged(string newBaseURI);
 
+    /// @notice Initializes the contract with given parameters.
+    /// @param baseURI_ The initial base URI for token metadata.
+    /// @param name The name of the token.
+    /// @param symbol The symbol of the token.
     constructor(
-        string memory baseURI,
+        string memory baseURI_,
         string memory name,
         string memory symbol
     ) ERC721(name, symbol) EIP712(name, "1") {
         _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
         _grantRole(MINTER_ROLE, msg.sender);
 
-        baseURI = baseURI;
-        emit BaseURIChanged(baseURI);
+        _baseURI = baseURI_;
+        emit BaseURIChanged(_baseURI);
     }
 
-    function _baseURI() internal pure override returns (string memory) {
-        return baseURI;
+    /// @dev Internal function to return the base URI for token metadata.
+    /// @return The base URI.
+    function _baseURI() internal view override returns (string memory) {
+        return _baseURI;
     }
 
+    /// @notice Sets a new base URI for token metadata.
+    /// @dev Restricted to accounts with the default admin role.
+    /// @param newBaseURI The new base URI to set.
     function setBaseURI(string memory newBaseURI) public onlyRole(DEFAULT_ADMIN_ROLE) {
-        baseURI = newBaseURI;
-        emit BaseURIChanged(baseURI);
+        _baseURI = newBaseURI;
+        emit BaseURIChanged(_baseURI);
     }
 
+    /// @notice Mints a new token with a specified URI to a given address.
+    /// @dev Restricted to accounts with the minter role.
+    /// @param to The address to mint the token to.
+    /// @param uri The URI for the new token's metadata.
     function safeMint(address to, string memory uri) public onlyRole(MINTER_ROLE) {
         uint256 tokenId = _nextTokenId++;
         _safeMint(to, tokenId);
@@ -46,6 +62,11 @@ contract ERC721Token is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnab
 
     // The following functions are overrides required by Solidity.
 
+    /// @dev Internal function to update delegatee for a token.
+    /// @param to The address to delegate to.
+    /// @param tokenId The token ID.
+    /// @param auth The authorized address.
+    /// @return The address of the delegatee.
     function _update(
         address to,
         uint256 tokenId,
@@ -54,14 +75,25 @@ contract ERC721Token is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnab
         return super._update(to, tokenId, auth);
     }
 
+    /// @dev Internal function to increase the balance of an account for voting.
+    /// @param account The account to increase the balance of.
+    /// @param value The value to increase the balance by.
     function _increaseBalance(address account, uint128 value) internal override(ERC721, ERC721Enumerable, ERC721Votes) {
         super._increaseBalance(account, value);
     }
 
+    /// @notice Returns the URI for a given token ID.
+    /// @dev Overrides ERC721 and ERC721URIStorage.
+    /// @param tokenId The token ID.
+    /// @return The URI of the given token ID.
     function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
         return super.tokenURI(tokenId);
     }
 
+    /// @notice Checks if the contract supports an interface.
+    /// @dev Overrides ERC721, ERC721Enumerable, ERC721URIStorage, and AccessControl.
+    /// @param interfaceId The interface ID to check.
+    /// @return True if the contract supports the interface.
     function supportsInterface(
         bytes4 interfaceId
     ) public view override(ERC721, ERC721Enumerable, ERC721URIStorage, AccessControl) returns (bool) {
--- tasks/taskDeploy.ts
+++ tasks/taskDeploy.ts
@@ -56,3 +56,62 @@ task("task:deployTimelock")
     await timelock.waitForDeployment();
     console.log("Timelock deployed to: ", await timelock.getAddress());
   });
+
+task("task:deployNFTDAO")
+  .addParam("name", "Token Name")
+  .addParam("symbol", "Token Symbol")
+  .addParam("baseURI", "Base URI")
+  .addParam("governorName", "Governor Name")
+  .addParam("votingPeriod", "Voting Period")
+  .addParam("votingDelay", "Voting Delay")
+  .addParam("proposalThreshold", "Proposal Threshold")
+  .addParam("percentageQuorum", "Percentage Quorum")
+  .addParam("minDelay", "Min Delay")
+  .addParam("proposers", "Proposers")
+  .addParam("executors", "Executors")
+  .addParam("admin", "Admin")
+  .setAction(async function (taskArguments: TaskArguments, { ethers }) {
+    const signers = await ethers.getSigners();
+
+    const tokenFactory = await ethers.getContractFactory("ERC721Token");
+    const governorFactory = await ethers.getContractFactory("Governor");
+    const timelockFactory = await ethers.getContractFactory("Timelock");
+
+    const { name,
+      symbol,
+      baseURI,
+      governorName,
+      votingPeriod,
+      votingDelay,
+      proposalThreshold,
+      percentageQuorum,
+      minDelay,
+      proposers,
+      executors,
+      admin } = taskArguments;
+
+    const token = await tokenFactory.connect(signers[0]).deploy(baseURI, name, symbol);
+    await token.waitForDeployment();
+
+    const timelock = await timelockFactory.connect(signers[0]).deploy(minDelay, proposers, executors, admin);
+    await timelock.waitForDeployment();
+
+    const governor = await governorFactory.connect(signers[0]).deploy(await token.getAddress, await timelock.getAddress(), governorName, votingPeriod, votingDelay, proposalThreshold, percentageQuorum);
+    await governor.waitForDeployment();
+
+    // Token
+    // mint tokens based on settings
+    // renounce the minter role
+    // renounce the default admin roll
+    // give default admin to governor
+
+    // Timelock
+    // Set the governor as the admin
+    // Set the governor as the super admin
+    // Set the governor as the proposer
+    // Set the governor as the executor
+    // renounce the admin role
+
+
+
+  });
